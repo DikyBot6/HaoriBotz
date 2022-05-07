6f49de31949d0bb4427dbdd3f8a8acead3571e6e
DikyBot6 <104796331+DikyBot6@users.noreply.github.com>
2022-05-07 10:33:50 +0700

    creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -3,11 +3,11 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "Zɪᴠғᴜʀʀ Cʜᴀɴ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Zivfurr\nitem1.TEL;waid=6285158866902:6282186172016\nitem1.X-ABLabel:📍 Creator Haoribotz\nitem2.EMAIL;type=INTERNET:haoribotz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3JH6rSB/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | Bengkulu\nitem5.X-ABLabel:──────[ Zivfurr ]──────\nEND:VCARD"
+    "displayName": "Diky Owner",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Dikybotz\nitem1.TEL;waid=62082122978010:6282122978010\nitem1.X-ABLabel:📍Owner Di ky\nitem2.EMAIL;type=INTERNET:dikybot36@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3JH6rSB/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | Jawa Timur\nitem5.X-ABLabel:──────[ Owner Diky]──────\nEND:VCARD"
   }, {
-    "displayName": "Yᴀɴᴢ Bᴏᴛ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Yanz Botz\nitem1.TEL;waid=6282186172016:0\nitem1.X-ABLabel:📍 Partner Haori\nitem2.EMAIL;type=INTERNET:yannzbotz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3JH6rSB/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | Sumatra Selatan\nitem5.X-ABLabel:────[ Yᴀɴᴢ Bᴏᴛᴢ ]────\nEND:VCARD"
+    "displayName": "Diky Botz",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Diky Botz\nitem1.TEL;waid=62895366380771:0\nitem1.X-ABLabel:📍 DikyBotz\nitem2.EMAIL;type=INTERNET: dikybot36@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3JH6rSB/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | Jawa Timur\nitem5.X-ABLabel:────[ Diky Bᴏᴛᴢ ]────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
